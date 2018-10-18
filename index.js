c54c101cf6acb078c3cb0f7aacc408570b516762
adamnpeace <adam.n.peace@gmail.com>
2018-10-18 11:53:59 +0100

    Added message

--- index.js
+++ index.js
@@ -3,16 +3,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-//const layout = require('express-layout')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const validator = require('express-validator')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
-//const flash = require('express-flash')
 const helmet = require('helmet')
-//const mysql = require('mysql')
-//const myConnection = require('express-myconnection')
 const methodOverride = require('method-override')
 
 
@@ -23,42 +19,7 @@ app.use('/', express.static(__dirname))
 // Set routes
 const index = require('./routes/index')
 
-// Set up object to send to client devices
-app.locals.var = []
-// Allow above variable to be used by routes
-/*
-const middlewares = [
-	helmet(),
-	//layout(),
-  express.static(path.join(__dirname, 'public')),
-	myConnection(mysql, dbOptions, 'pool'),
-  bodyParser.urlencoded({
-	extended: true}),
-	bodyParser.json(),
-  validator(),
-  cookieParser('super-secret-key'),
-  session({
-    secret: 'super-secret-key',
-    key: 'super-secret-cookie',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60000 }
-  }),
-  flash(),
-	methodOverride(function (req) {
-		if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-			// look in urlencoded POST bodies and delete it
-			var method = req.body._method
-			delete req.body._method
-			return method
-		}
-	}),
-]
-*/
-//app.use(middlewares)
 app.use('/', index)
-//app.use('/index', host)
-//app.use('/client', client)
 
 app.use((req, res, next) => {
   res.status(404).send("Turn around now! Nothing exists under this URL")
